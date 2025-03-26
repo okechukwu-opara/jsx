74385c494f85d6feb3ca7af65dc685bba5659a52
okechukwu-opara <oparachike@gmail.com>
2025-03-26 19:28:41 +0100

    Added build directory for GitHub Pages deployment

---
+++ seasons/build/precache-manifest.bb3c0af47a88f9a39801abfe114846bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c70b957e123785503fc3e0a25169159",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "5d6806d8146fa627b6ea",
+    "url": "/seasons/static/css/main.50aee88f.chunk.css"
+  },
+  {
+    "revision": "fc8452304b106829cb25",
+    "url": "/seasons/static/js/2.3b045380.chunk.js"
+  },
+  {
+    "revision": "5d6806d8146fa627b6ea",
+    "url": "/seasons/static/js/main.8c78afe4.chunk.js"
+  },
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  }
+]);
