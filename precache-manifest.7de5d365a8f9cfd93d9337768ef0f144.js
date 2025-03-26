c781eb6a8cd7ee106d1dd7c56cc0234e5229e0fc
okechukwu-opara <oparachike@gmail.com>
2025-03-26 19:04:56 +0100

    Updates

---
+++ precache-manifest.7de5d365a8f9cfd93d9337768ef0f144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196cea93314ca7f8c2a7099d7701e2eb",
+    "url": "/jsx/seasons/index.html"
+  },
+  {
+    "revision": "5d6806d8146fa627b6ea",
+    "url": "/jsx/seasons/static/css/main.50aee88f.chunk.css"
+  },
+  {
+    "revision": "fc8452304b106829cb25",
+    "url": "/jsx/seasons/static/js/2.3b045380.chunk.js"
+  },
+  {
+    "revision": "5d6806d8146fa627b6ea",
+    "url": "/jsx/seasons/static/js/main.8c78afe4.chunk.js"
+  },
+  {
+    "revision": "2b3245a52bf3aa7d8ab3",
+    "url": "/jsx/seasons/static/js/runtime~main.a6cd031c.js"
+  }
+]);
